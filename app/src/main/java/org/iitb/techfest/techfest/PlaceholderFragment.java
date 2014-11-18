9c28c6a548e4efb746d109564eb0b08f43ad0241
Meet Udeshi <mudeshi1209@gmail.com>
2014-11-18 07:06:27 +0530

    Fix minor bugs and move PlaceholderFragment to another file
    XML having problem with '<>' inside string
    
    We need to use getArguments() for the bundle instead of
    directly using savedInstanceState

---
+++ app/src/main/java/org/iitb/techfest/techfest/PlaceholderFragment.java
@@ -0,0 +1,55 @@
+package org.iitb.techfest.techfest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class PlaceholderFragment extends Fragment {
+    /**
+     * The fragment argument representing the section number for this
+     * fragment.
+     */
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    /**
+     * Returns a new instance of this fragment for the given section
+     * number.
+     */
+    public static PlaceholderFragment newInstance(int sectionNumber) {
+        PlaceholderFragment fragment = new PlaceholderFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public PlaceholderFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        Bundle args=getArguments();
+
+        int section_number=args.getInt(ARG_SECTION_NUMBER);
+        int layout=R.layout.fragment_main;
+        if(section_number==2)
+            layout=R.layout.fragment_competitions;
+
+        View rootView = inflater.inflate(layout, container, false);
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        ((MainActivity) activity).onSectionAttached(
+                getArguments().getInt(ARG_SECTION_NUMBER));
+    }
+}
