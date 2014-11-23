3e296c171f2df51cc80dd69aa454a8419636f611
Meet Shah <meetshah1995@iitb.ac.in>
2014-11-23 19:42:39 +0530

    navigation files added

---
+++ app/src/main/java/org/iitb/techfest/techfest/HttpConnection.java
@@ -0,0 +1,75 @@
+package org.iitb.techfest.techfest;
+
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class HttpConnection {
+
+    public String readUrl(String mapsApiDirectionsUrl) throws IOException {
+        String data = "";
+        InputStream iStream = null;
+        HttpURLConnection urlConnection = null;
+        try {
+            URL url = new URL(mapsApiDirectionsUrl);
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setInstanceFollowRedirects(true);
+            urlConnection.connect();
+            iStream = urlConnection.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    iStream));
+            StringBuffer sb = new StringBuffer();
+            String line = "";
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+            data = sb.toString();
+            br.close();
+        } catch (Exception e) {
+            Log.d("Exception while reading url", e.toString());
+        } finally {
+            iStream.close();
+            urlConnection.disconnect();
+        }
+        return data;
+    }
+
+    public List<String[]> getCSV(String csvUrl) throws IOException {
+
+        List<String[]> data=new ArrayList<String[]>();
+        InputStream iStream = null;
+        HttpURLConnection urlConnection = null;
+        try {
+            URL url = new URL(csvUrl);
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setInstanceFollowRedirects(true);
+            urlConnection.connect();
+
+            Log.d("Download Debug","response: " + urlConnection.getResponseCode()+" -> "+urlConnection.getResponseMessage());
+            iStream = urlConnection.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    iStream));
+            String line = "";
+            while ((line = br.readLine()) != null) {
+                String[] next = line.split(",");
+                Log.i("Download Debug","row recieved : "+next[0]);
+                data.add(next);
+            }
+            Log.i("Download Debug","data size : "+data.size());
+        } catch (Exception e) {
+            Log.d("Exception while reading url", e.toString());
+        } finally {
+            iStream.close();
+            urlConnection.disconnect();
+        }
+
+        return data;
+    }
+}
