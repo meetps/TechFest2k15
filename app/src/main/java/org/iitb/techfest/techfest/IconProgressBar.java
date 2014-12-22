0a22c6912e804965858978df7b8284d9b033324a
Meet Udeshi <mudeshi1209@gmail.com>
2014-12-22 14:43:49 +0530

    initial code for progressbar

---
+++ app/src/main/java/org/iitb/techfest/techfest/IconProgressBar.java
@@ -0,0 +1,39 @@
+package org.iitb.techfest.techfest;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+
+public class IconProgressBar extends View {
+    Bitmap front, back;
+    int progress=0;
+
+    public IconProgressBar(Context context, AttributeSet attrs) {
+        super(context);
+
+        front = BitmapFactory.decodeResource(context.getResources(),R.drawable.tf_front);
+        back = BitmapFactory.decodeResource(context.getResources(),R.drawable.tf_back);
+    }
+
+    @Override
+    public void onDraw(Canvas canvas){
+        try{
+            canvas.drawBitmap(Bitmap.createScaledBitmap(Bitmap.createBitmap(back,0,(100-progress)*back.getHeight()/100,back.getWidth(),progress*back.getHeight()/100),canvas.getWidth(),canvas.getHeight(),true),0,(100-progress)*canvas.getHeight()/100,null);
+        } catch (IllegalArgumentException e){
+
+        }
+
+        canvas.drawBitmap(Bitmap.createScaledBitmap(front, canvas.getWidth(), canvas.getHeight(), true), 0, 0, null);
+    }
+
+    public void updateProgress(int progress){
+        this.progress = progress;
+
+        Log.d("ProgressBar", "updateProgress called "+progress);
+        invalidate();
+    }
+}
--- app/src/main/java/org/iitb/techfest/techfest/SplashActivity.java
+++ app/src/main/java/org/iitb/techfest/techfest/SplashActivity.java
@@ -6,15 +6,17 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.Toast;
+import android.widget.TextView;
 
-
-import java.io.BufferedWriter;
+import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,22 +24,36 @@ import java.util.List;
 public class SplashActivity extends Activity {
     List<String[]> data;
     ArrayList<EventSummary> converted=new ArrayList<EventSummary>();
+    IconProgressBar loader;
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        loader=(IconProgressBar)findViewById(R.id.splash_loader);
+        text = (TextView)findViewById(R.id.percent_text);
+
         new ReadTask().execute("https://drive.google.com/uc?execute=download&id=0B_6rvZNWXShpMi03TkR1MWxwUGM");
     }
 
     private class ReadTask extends AsyncTask<String, Integer, List<String[]>> {
+
+        @Override
+        protected void onPreExecute(){
+            super.onPreExecute();
+            loader = (IconProgressBar)findViewById(R.id.splash_loader);
+            text = (TextView)findViewById(R.id.percent_text);
+
+            Log.d("ProgressBar","onPreExecute : "+loader);
+        }
+
         @Override
         protected List<String[]> doInBackground(String... url) {
             List<String[]> data = new ArrayList<String[]>();
             try {
-                HttpConnection http = new HttpConnection();
-                data = http.downloadCSV(url[0]);
+                data = downloadCSV(url[0]);
 
                 if(data!=null){
                     try {
@@ -57,13 +73,14 @@ public class SplashActivity extends Activity {
                 } else {
                     try {
                         FileInputStream in = openFileInput("events.csv");
-                        data=HttpConnection.parseCSV(in);
+                        data=parseCSV(in,in.getChannel().size());
                         Log.d("Cache Load","Loaded from app cache");
                     } catch (Exception e) {
                         Log.e("Cache Load",e.getMessage());
 
                         try {
-                            data=HttpConnection.parseCSV(getAssets().open("events.csv"));
+                            InputStream asset = getAssets().open("events.csv");
+                            data=parseCSV(asset,asset.available());
                             Log.d("Cache Load","Loaded from assets");
                         } catch (IOException e1) {
                             Log.e("Cache Load", e1.getMessage());
@@ -79,7 +96,12 @@ public class SplashActivity extends Activity {
 
         @Override
         protected void onProgressUpdate(Integer... progress) {
-            Log.d("Download Debug", "progress:" + progress);
+            Log.d("Download Debug", "progress:" + progress[0]);
+
+            if(loader!=null)
+                loader.updateProgress(progress[0]);
+
+            text.setText("Loading events "+progress[0]+"%");
         }
 
         @Override
@@ -87,6 +109,55 @@ public class SplashActivity extends Activity {
             super.onPostExecute(result);
             data=result;
             startMainActivity();
+        }
+
+        public List<String[]> downloadCSV(String csvUrl) throws IOException {
+            List<String[]> data=null;
+            InputStream iStream = null;
+            HttpURLConnection urlConnection = null;
+            try {
+                URL url = new URL(csvUrl);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setInstanceFollowRedirects(true);
+                urlConnection.connect();
+
+                Log.d("Download Debug","response: " + urlConnection.getResponseCode()+" -> "+urlConnection.getResponseMessage());
+                iStream = urlConnection.getInputStream();
+
+                data=parseCSV(iStream, urlConnection.getContentLength());
+            } catch (Exception e) {
+                Log.d("Exception while reading url", e.toString());
+            } finally {
+                if(iStream!=null) iStream.close();
+                urlConnection.disconnect();
+
+                return data;
+            }
+        }
+
+        public List<String[]> parseCSV(InputStream in, long streamLength) throws IOException{
+            List<String[]> data=new ArrayList<String[]>();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    in));
+
+            long loaded=0;
+
+            String line = "";
+            while ((line = br.readLine()) != null) {
+                loaded+=line.length();
+                String[] next = line.split(";");
+                Log.i("Download Debug","row recieved : "+next[0]);
+                data.add(next);
+
+                publishProgress((int)((double)loaded/streamLength*100));
+
+            }
+            Log.i("Download Debug","data size : "+data.size());
+
+            in.close();
+
+            return data;
         }
     }
 
